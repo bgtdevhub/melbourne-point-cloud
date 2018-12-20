dcf374a3bb34caf2190ea00ab82360f78b41fa95
Lucas Lew <llew@bousteadgeospatial.com>
2018-12-20 13:22:07 +0800

    added city of melbourne logo

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -6,6 +6,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <div className="com-logo-div">
+          <img src="img/com-logo.png"
+            alt="City of Melbourne logo"
+            className="com-logo"
+          ></img>
+        </div>
         <SceneView />
       </div>
     );
